510aaa25380be9b9aff24732ace2052fff485b91
musishere <mustafabukhari333@gmail.com>
2025-08-30 16:21:51 +0500

    Added proxy to redirect hi to 3000 to 3001

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -1,21 +1,64 @@
 import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
 import helmet from "helmet";
+import Redis from "ioredis";
+import { RateLimiterRedis } from "rate-limiter-flexible";
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT;
+const redisClient = new Redis(process.env.REDIS_URL);
+const proxyOptions = {
+  proxyReqPathResolver: (req) => {
+    return req.originalUrl.replace(/\/v1/, "api");
+  },
+  proxyErrorHandler: (err, res, next) => {
+    if (err.code === "ECONNREFUSED") {
+      logger.error("Service Unavailable", {
+        service: err.host,
+        error: err.message,
+      });
+      res.status(500).json({ error: "Service Unavailable" });
+    } else {
+      next(err);
+    }
+  },
+};
 
+app.use("v2/auth", proxy(process.env.IDENTITY_SERVICE_URL, ...proxyOptions), {
+  proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+    proxyReqOpts.headers["Content-Type"] = "application/json";
+    return proxyReqOpts;
+  },
+  userResDecorator: (proxyRes, srcReq, res) => {
+    res.setHeader("X-RateLimit-Limit", proxyRes.headers["x-ratelimit-limit"]);
+    res.setHeader(
+      "X-RateLimit-Remaining",
+      proxyRes.headers["x-ratelimit-remaining"]
+    );
+    res.setHeader("X-RateLimit-Reset", proxyRes.headers["x-ratelimit-reset"]);
+    return proxyRes;
+  },
+});
 app.use(cors());
-app.use(helmet());
 app.use(express.json());
+app.use(helmet());
 
-// global rate limiter
-const globalRateLimiter = new RateLimiterRedis({
-  storeClient: redis,
-  keyPrefix: "global",
-  points: 60, // allow 60 requests
-  duration: 1, // per 1 second
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.url} ${res.statusCode}`);
+  next();
 });
 
+// Global Rate Limiter
+const globalRateLimiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: "global",
+  points: 60,
+  duration: 1,
+});
+
+// track the global rate limiter
 app.use(async (req, res, next) => {
   try {
     const rlRes = await globalRateLimiter.consume(req.ip);
@@ -37,14 +80,16 @@ app.use(async (req, res, next) => {
     });
   }
 });
+
+// rate limiter for sensitive endpoints
 const sensitiveRateLimiter = new RateLimiterRedis({
-  storeClient: redis,
+  storeClient: redisClient,
   keyPrefix: "sensitive",
-  points: 50, // max 50 requests
-  duration: 15 * 60, // per 15 minutes
+  points: 60,
+  duration: 1,
 });
 
-// middleware for sensitive routes
+// track the sensitive endpoints rate limiter
 const sensitiveEndpointsRateLimit = async (req, res, next) => {
   try {
     await sensitiveRateLimiter.consume(req.ip);
@@ -57,6 +102,10 @@ const sensitiveEndpointsRateLimit = async (req, res, next) => {
     });
   }
 };
-app.listen(3000, () => {
-  console.log("API Gateway is running on port 3000");
+
+// start the server
+app.listen(port, () => {
+  logger.info(`API Gateway is running on port ${port}`);
 });
+
+export default app;
